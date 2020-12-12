b74493926405fdbafd301f29635fc696b59836e6
Mason Egger <mason@masonegger.com>
2020-12-12 11:39:12 -0600

    disabling the tree rebuild for a bit to see if it's necessary

--- fraternity_tree/views.py
+++ fraternity_tree/views.py
@@ -5,74 +5,94 @@ from fraternity_tree.models import Brother, PledgeClass
 
 from .forms import FamilyTreeForm, PledgeClassForm
 
+
 def index(request):
-    Brother.objects.rebuild()
-    return  render(request, 'fraternity_tree/tree.html',
-    {
-        'brothers': Brother.objects.all(),
-    },)
+    # Brother.objects.rebuild()
+    return render(
+        request,
+        "fraternity_tree/tree.html",
+        {
+            "brothers": Brother.objects.all(),
+        },
+    )
 
 
 def my_tree(request):
     brother_list = []
-    tmp_list = Brother.objects.all().order_by('last_name', 'first_name', 'pledge_class__date_initiated')
+    tmp_list = Brother.objects.all().order_by(
+        "last_name", "first_name", "pledge_class__date_initiated"
+    )
     for brother in tmp_list:
         brother_list.append((brother.id, brother.__str__()))
-    if request.method == 'POST':
-            form = FamilyTreeForm(request.POST, data_list=brother_list)
-            print("POST")
-            if form.is_valid():
-                print("VALID")
-                try:
-                    brother_id = form.cleaned_data['name']
-                    generations = form.cleaned_data['generations']
-                    if generations is None:
-                        generations = 0
-                    brother = Brother.objects.get(pk=brother_id)
-                    if generations > 0:
-                        brother = brother.get_ancestors()
-                        if generations >= len(brother):
-                            brother = brother[0]
-                        else:
-                            brother = brother[len(brother) - generations]
+    if request.method == "POST":
+        form = FamilyTreeForm(request.POST, data_list=brother_list)
+        if form.is_valid():
+            try:
+                brother_id = form.cleaned_data["name"]
+                generations = form.cleaned_data["generations"]
+                if generations is None:
+                    generations = 0
+                brother = Brother.objects.get(pk=brother_id)
+                if generations > 0:
+                    brother = brother.get_ancestors()
+                    if generations >= len(brother):
+                        brother = brother[0]
+                    else:
+                        brother = brother[len(brother) - generations]
 
-                    return render(request,'fraternity_tree/tree.html',
-                    {
-                        'brothers': brother.get_descendants(include_self=True)
-                    },)
-                except Exception as e:
-                    return HttpResponse("Exception %s was thrown. Report this error to the site adminstrator" % e)
+                return render(
+                    request,
+                    "fraternity_tree/tree.html",
+                    {"brothers": brother.get_descendants(include_self=True)},
+                )
+            except Exception as e:
+                return HttpResponse(
+                    "Exception %s was thrown. Report this error to the site adminstrator"
+                    % e
+                )
     else:
         form = FamilyTreeForm(data_list=brother_list)
-    return render(request,
-    'fraternity_tree/my_tree.html',
-    {
-        'form': form,
-    },)
+    return render(
+        request,
+        "fraternity_tree/my_tree.html",
+        {
+            "form": form,
+        },
+    )
+
 
 def pledge_class(request):
     pledge_classes = []
-    pc = PledgeClass.objects.all().order_by('date_initiated')
+    pc = PledgeClass.objects.all().order_by("date_initiated")
     for pclass in pc:
         pledge_classes.append((pclass.get_name(), pclass.get_name()))
-    if request.method == 'POST':
-            form = PledgeClassForm(request.POST, data_list=pledge_classes)
-            if form.is_valid():
-                #try:
-                pledge_class = form.cleaned_data['pledge_class']
-                print(pledge_class)
-                pledge_class = PledgeClass.objects.get(name=pledge_class)
-                pledge_brothers = Brother.objects.all().filter(pledge_class=pledge_class).order_by('last_name', 'first_name')
-                return render(request,'fraternity_tree/pledge_class.html',
+    if request.method == "POST":
+        form = PledgeClassForm(request.POST, data_list=pledge_classes)
+        if form.is_valid():
+            # try:
+            pledge_class = form.cleaned_data["pledge_class"]
+            print(pledge_class)
+            pledge_class = PledgeClass.objects.get(name=pledge_class)
+            pledge_brothers = (
+                Brother.objects.all()
+                .filter(pledge_class=pledge_class)
+                .order_by("last_name", "first_name")
+            )
+            return render(
+                request,
+                "fraternity_tree/pledge_class.html",
                 {
-                    'pledge_brothers': pledge_brothers,
-                },)
-                #except Exception as e:
-                #    return HttpResponse("Exception %s was thrown. Report this error to the site adminstrator" % e)
+                    "pledge_brothers": pledge_brothers,
+                },
+            )
+            # except Exception as e:
+            #    return HttpResponse("Exception %s was thrown. Report this error to the site adminstrator" % e)
     else:
         form = PledgeClassForm(data_list=pledge_classes)
-    return render(request,
-    'fraternity_tree/my_pledge_class.html',
-    {
-        'form': form,
-    },)
+    return render(
+        request,
+        "fraternity_tree/my_pledge_class.html",
+        {
+            "form": form,
+        },
+    )
